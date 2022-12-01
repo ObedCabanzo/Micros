81d4e7a39e96ffc28eef97982549f6279b4ac629
obedCabanzo <jo.cabanzo@uniandes.edu.co>
2022-11-30 20:41:11 -0500

    cambios

--- places/places/views.py
+++ places/places/views.py
@@ -1,11 +1,6 @@
 from .models import Place
-from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.http import HttpResponse
 from django.http import JsonResponse
-from django.urls import reverse
-from django.conf import settings
-import requests
 import json
 
 
